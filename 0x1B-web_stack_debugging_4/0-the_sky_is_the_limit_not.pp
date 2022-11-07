dcad93af9aeaaed3959eba6535da7fbc7af52878
Zandarh <ubaksxander@gmail.com>
2022-11-07 08:07:33 +0100

    Debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increae the amount of traffic an Nginx server can handle
+
+# Increae the ULIMIT of hthe default file
+exec { 'fix--for-nginx':
+	command => 'sed -i "s/15/4096/" /etc/default/nginx',
+	path    => '/usr/local/bin/:bin/'
+} ->
+
+# Restart the Nginx server
+exec { 'nginx-restart':
+	command => 'nginx restart',
+	path    => '/etc/init.d/'
+}
